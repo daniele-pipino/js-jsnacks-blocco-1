ab8cee82e33d9016a83003ad3f81aed6ce476920
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-07 15:42:55 +0200

    sneak 1-svolgimento con ciclo for

--- js/sneak-1.js
+++ js/sneak-1.js
@@ -2,3 +2,20 @@
 Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while. */
 
+
+// sviluppo con for
+
+var somma = 0;
+
+for (var i = 0; i < 5; i++) {
+    var usernumber = parseInt(prompt('Inserisci 5 numeri'));
+    console.log(usernumber);
+    if (isNaN(usernumber) || usernumber < 0) {
+        alert('Il carattere da te inserito non è valido');
+    } else {
+        somma += usernumber;
+        console.log(somma);
+    }
+}
+
+// sviluppo con while
